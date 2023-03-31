50e5525eef2ce6859276ec7beabe9e363c4ad4c5
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-03-31 12:16:01 +0530

    product add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,78 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react';
+import ReadMoreReact from 'read-more-react';
+import { Link } from 'react-router-dom';
 const Home = (props) => {
-  
+
+  const [categories, setCategories] = useState([]);
+  const [products, setProduct] = useState([]);
+
+  const getCategories = () => {
+    fetch('https://fakestoreapi.com/products/categories')
+      .then(res => res.json())
+      .then(json => setCategories(json));
+  }
+
+  const getAllProduct = () => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(json => setProduct(json))
+  }
+
+  useEffect(() => {
+    getCategories();
+    getAllProduct();
+  }, []);
   return (
     <div>
-     <h>Home</h>
-</div>
+      <nav className="navbar navbar-expand-lg navbar-light ">
+        <div style={{ width: "100%" }}>
+          <ul className="navbar-nav  d-flex justify-content-around">
+            {
+              categories.map((item) => {
+                return (
+                  <li className="nav-item">
+                    <button type="button" >{item}</button>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+      </nav>
+
+      <div className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
+        <ul className="showproduct">
+          {
+            products.map((item) => {
+              return (
+                <li className="nav-item">
+                  <div className="card" style={{ width: "14rem",margin:"10px" }}>
+                    <img className="card-img-top" src={item.image} alt="Card image cap" />
+                    <div className="card-body">
+                      <h3 className="card-price" >${item.price}</h3>
+                      <h5 className="card-title">{item.title.split(' ').slice(0,4).join(' ')}</h5>
+                   
+                      <ReadMoreReact text={item.description}
+                        min={50}
+                        ideal={50}
+                        max={100}
+                        readMoreText={"read more"} />
+
+                      {/* <a href="#" className="btn btn-primary">Add to Cart</a> */}
+                      <div className='btnCard' >
+                        <button  class="btn  btn-sm btn-warning">Add to Cart</button>
+                        <button  class="btn btn-sm btn-info">Buy Now</button>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link,useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 
 const Navbar = (props) => {
-    const history=useHistory();
+    const history = useHistory();
 
-    const handlelogout=(event)=>{
+    const handlelogout = (event) => {
         event.preventDefault();
         props.setUser('');
         localStorage.clear();
@@ -17,29 +17,44 @@ const Navbar = (props) => {
     return (
 
         <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
-                <h1 className="navbar-brand" >E-cart</h1>
+            <h1 className="navbar-brand" >E-cart</h1>
 
-                <div>
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to={'/about'}>AboutUs</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to={'/contact'}>ContactUs</Link>
-                        </li>
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to={'/about'}>AboutUs</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to={'/contact'}>ContactUs</Link>
+                    </li>
 
-                        <li className="nav-item">
+                    <li className="nav-item">
                         <p className="nav-link"> {props.name}</p>
-                        </li>
-                        
-                        <li className="nav-item">
-                        <button type="button" onClick={(e)=>handlelogout(e)} class="btn btn-danger">Logout</button>
-                        </li>
-                    </ul>
-                </div>
+
+
+                    </li>
+
+                    <li className='nav-item' style={{margin:"8px"}}>
+                    <p style={{ border: "none", background: "none", cursor: "pointer" }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                        </svg>
+                        <span className='badge badge-warning' id='lblCartCount'> 5 </span>
+                    </p>
+                    </li>
+                   
+
+
+                    <li className="nav-item">
+                        <button type="button" onClick={(e) => handlelogout(e)} class="btn btn-danger">Logout</button>
+                    </li>
+
+
+                </ul>
+            </div>
         </nav>
 
     )
